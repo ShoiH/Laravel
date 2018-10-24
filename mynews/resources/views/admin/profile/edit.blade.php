00b6f2c6d4b8d69a19f218c327c91722e68233cf
Shoi Hasegawa <wdlove1008sekai@gmail.com>
2018-10-24 17:41:46 +0900

    add files 15

--- mynews/resources/views/admin/profile/edit.blade.php
+++ mynews/resources/views/admin/profile/edit.blade.php
@@ -12,6 +12,7 @@
             <div class="col-md-8 mx-auto">
                 <h2>My プロフィール</h2>
                 <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+                
                 @if (count($errors) > 0)
                     <ul>
                         @foreach($errors->all() as $e)
